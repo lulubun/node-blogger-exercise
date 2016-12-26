7f064bf027b607836f79bb611791e91acf265346
lulubun <laurapetitt@gmail.com>
2016-12-25 23:13:57 -0500

    undid edits

--- test/tests.js
+++ test/tests.js
@@ -71,13 +71,11 @@ describe('BlogPosts', function() {
       	})
 
       	.then(function(res) {
-        	res.should.have.status(201);
+        	res.should.have.status(200);
         	res.should.be.json;
         	res.body.should.be.a('object');
-        	res.body.should.include.keys('id', 'title', 'content', 'author', 'publishDate');
-        	res.body.id.should.not.be.null;
-        	res.body.should.deep.equal(Object.assign(newItem, {id: res.body.id}));
-      	});
+        	res.body.should.deep.equal(updateData);
+       	});
     });
 
   	it('should delete items on DELETE', function() {
